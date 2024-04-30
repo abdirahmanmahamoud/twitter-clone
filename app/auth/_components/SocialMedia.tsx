9b32e5d66078e1e0f1a7d83923f62436726b43e8
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2024-04-30 14:23:40 +0300

    add provider google & github

--- app/auth/_components/SocialMedia.tsx
+++ app/auth/_components/SocialMedia.tsx
@@ -1,12 +1,21 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { signIn } from "next-auth/react";
 
 export const SocialMedia = () => {
+  const handleSingIn = (provider: string) => {
+    signIn(provider, {
+      callbackUrl: "/",
+    });
+  };
+
   return (
     <div className='w-full space-y-3'>
       <Button
         className='w-full flex items-center justify-center space-x-2'
         variant='outline'
+        onClick={() => handleSingIn("google")}
       >
         <FaGoogle className='w-4 h-4' />
         <span className='text-[1rem]'>Login with google</span>
@@ -14,6 +23,7 @@ export const SocialMedia = () => {
       <Button
         className='w-full flex items-center justify-center space-x-2'
         variant='outline'
+        onClick={() => handleSingIn("github")}
       >
         <FaGithub className='w-4 h-4' />
         <span className='text-[1rem]'>Login with github</span>
