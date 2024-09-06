b056ba6f768ed022eab1ca347e9bdfc4a78c9673
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2024-09-06 19:28:24 +0300

    fix navbar fixed

--- app/(sitePages)/layout.tsx
+++ app/(sitePages)/layout.tsx
@@ -4,10 +4,12 @@ const SiteLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='px-0 lg:px-[8%] mx-auto'>
       <div className='w-full flex'>
-        <div className='w-[9%] lg:w-[18%] h-screen'>
+        <div className='w-[9%] lg:w-[18%] h-screen fixed'>
           <NavBar />
         </div>
-        <div className='w-[91%] lg:w-[82%]'>{children}</div>
+        <div className='w-[91%] lg:w-[82%] ml-[9%] lg:ml-[21.4%]'>
+          {children}
+        </div>
       </div>
     </div>
   );
